8f01038b7c7b4ebe1405d58df2a7bf9273c62921
juancabana <juand.cabanat@unac.edu.co>
2023-01-25 09:02:39 -0500

    routing

---
+++ src/router/router.jsx
@@ -0,0 +1,14 @@
+import Home from "../pages/Home.jsx";
+import Pokemon from "../pages/Pokemon.jsx";
+
+import { createBrowserRouter } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  { path: "/pokemon", element: <Pokemon /> },
+]);
+
+export default router;
